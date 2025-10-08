60d5f795bb081cd0f7c61a4ed77dbb973737d374
Piotr Kosowicz <kosowicz.peter@gmail.com>
2025-10-08 15:02:59 +0200

    Added auth in accordance to the official Supabase documentation (Supabase will be used for both storage and auth).

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,26 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
-	import "../app.css";
-	import Footer from '../components/Footer.svelte';
+	import '../app.css';
 	import Header from '../components/Header.svelte';
 	import { watchResize } from '$lib/resizeHelper';
 	import { onMount } from 'svelte';
-	let { children } = $props();
+	import { invalidate } from '$app/navigation';
+
+	let { data, children } = $props();
+	let { session, supabase } = $derived(data);
 
 	onMount(() => {
 		const cleanup = watchResize();
-		return cleanup;
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => {
+			cleanup();
+			data.subscription.unsubscribe();
+		};
 	});
 </script>
 
@@ -19,7 +30,7 @@
 
 <Header />
 <div
-    class="container relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
+	class="container relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
 >
-    {@render children()}
+	{@render children()}
 </div>
