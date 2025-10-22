a7bb401920253049b96f8ce67f532711c5bac827
Piotr Kosowicz <kosowicz.peter@gmail.com>
2025-10-22 03:55:28 +0200

    prepared the skeleton for the /private page

--- src/routes/private/+layout.svelte
+++ src/routes/private/+layout.svelte
@@ -1,20 +1,8 @@
 <script lang="ts">
-    import type { LayoutData } from "../$types";
-    let { data, children } = $props<{data: LayoutData, children: any}>();
-    let { supabase } = $derived(data);
-    const logout = async () => {
-      const { error } = await supabase.auth.signOut();
-      if (error) {
-        console.error(error);
-      }
-    };
+	import type { LayoutData } from '../$types';
+	let { data, children } = $props<{ data: LayoutData; children: any }>();
 </script>
-<header>
-    <nav>
-      <a href="/">Home</a>
-    </nav>
-    <button onclick={logout}>Logout</button>
-</header>
+
 <main>
-    {@render children()}
+	{@render children()}
 </main>
--- src/routes/private/+page.svelte
+++ src/routes/private/+page.svelte
@@ -1,37 +1,48 @@
 <script lang="ts">
-    import { invalidate } from '$app/navigation'
-    import type { EventHandler } from 'svelte/elements'
-  
-    let { data } = $props()
-    let { notes, supabase, user } = $derived(data)
-  
-    const handleSubmit: EventHandler<SubmitEvent, HTMLFormElement> = async (evt) => {
-      evt.preventDefault()
-      if (!evt.target) return
-  
-      const form = evt.target as HTMLFormElement
-  
-      const note = (new FormData(form).get('note') ?? '') as string
-      if (!note) return
-  
-      const { error } = await supabase.from('notes').insert({ note })
-      if (error) console.error(error)
-  
-      invalidate('supabase:db:notes')
-      form.reset()
-    }
-  </script>
-  
-  <h1>Private page for user: {user?.email}</h1>
-  <h2>Notes</h2>
-  <ul>
-    {#each notes as note}
-      <li>{note.note}</li>
-    {/each}
-  </ul>
-  <form onsubmit={handleSubmit}>
-    <label>
-      Add a note
-      <input name="note" type="text" />
-    </label>
-  </form>
+	let { data } = $props();
+	let { user, supabase } = $derived(data);
+
+	const logout = async () => {
+		const { error } = await supabase.auth.signOut();
+		if (error) {
+			console.error(error);
+		}
+	};
+</script>
+
+<div class="">
+  <!-- name + log out -->
+	<div class="flex p-5 m-10 bg-base-300 justify-between rounded-xl">
+		<p class="text-3xl">{user?.email}</p>
+		<button class="btn btn-primary btn-outline" onclick={logout}>Log out</button>
+	</div>
+
+	<!-- navigation -->
+  <div class="flex">
+		<div class="rounded-xl p-5 bg-base-300 m-10">
+		</div>
+    <div class="rounded-xl p-5 bg-base-300 m-10">
+		</div>
+    <div class="rounded-xl p-5 bg-base-300 m-10">
+		</div>
+	</div>
+
+  <!-- stats -->
+  <div class="flex flex-col p-5 m-10 bg-base-300 justify-between rounded-xl">
+		<p>Member for:</p>
+    <p>Own recepies:</p>
+    <p>Saved recepies:</p>
+    ...
+	</div>
+
+	<!-- account option -->
+	<div class="rounded-xl p-5 bg-base-300 m-10">
+		<div class="flex gap-3 items-end justify-between">
+			<button class="btn btn-error btn-outline btn-xs" onclick={logout}>Delete account</button>
+			<div class="flex gap-3 justify-end items-end">
+				<button class="btn btn-outline" onclick={logout}>Change password</button>
+				<button class="btn btn-outline" onclick={logout}>Change e-mail</button>
+			</div>
+		</div>
+	</div>
+</div>
